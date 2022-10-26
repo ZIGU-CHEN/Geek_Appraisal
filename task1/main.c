30a9cf8c1e5a9e02efc2c64b65b28a2172b9a2c0
ZIGU <1690949391@qq.com>
2022-10-26 20:34:54 +0800

    Modify main.c

--- task1/main.c
+++ task1/main.c
@@ -2,7 +2,25 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+	double  x=0;
+	double  y=0;
+	double a = 0;
+	double h = 0.001;
+	double t = 0;
+	double c = 0;
+	printf("ÇëÊäÈëaµÄÖµ£º" );
+	scanf( " %lf",&a);
+	y = a;
+	t = a;
+	printf("x       y\n");
+   do 
+   {
+	   c = y;
+	   x += h;
+	   y = t + h * (-t);
+	   t = y;
+	   printf("%f     %f\n", x, y);
+   } while (c-t>0.000001);
 	return 0;
 }
 
